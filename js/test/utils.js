a18d0096d38154c70f18faea866a6461abd2e310
Hugh Lee <hammerha@gmail.com>
2016-05-06 23:52:15 +0900

    Add rgbToHex() test

--- js/test/utils.js
+++ js/test/utils.js
@@ -5,3 +5,10 @@
 function range(start, count) {
   return Array(count).fill().map((_, i) => start + i);
 }
+
+/**
+ * http://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+ */
+function rgbToHex(r, g, b) {
+  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
--- specs/utilsSpec.js
+++ specs/utilsSpec.js
@@ -1,6 +1,14 @@
 describe('Utils', function() {
-  it('should range() return range', function() {
+
+  it('range()', function() {
     expect(range(1, 3)).toEqual([1, 2, 3]);
   });
+
+  it('rgbToHex()', function() {
+    expect(rgbToHex(0, 0, 0)).toBe('#000000');
+    expect(rgbToHex(255, 255, 255)).toBe('#ffffff');
+    expect(rgbToHex(171, 205, 239)).toBe('#abcdef');
+  });
+
 });
 
