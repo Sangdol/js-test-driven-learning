646dbb401c96dcf433c32d7f645fe577852c412a
Hugh Lee <hammerha@gmail.com>
2016-05-07 22:26:52 +0900

    Add more rgbToHex test

--- specs/utilsSpec.js
+++ specs/utilsSpec.js
@@ -8,6 +8,10 @@ describe('Utils', function() {
     expect(rgbToHex(0, 0, 0)).toBe('#000000');
     expect(rgbToHex(255, 255, 255)).toBe('#ffffff');
     expect(rgbToHex(171, 205, 239)).toBe('#abcdef');
+
+    // not working well with float number
+    expect(rgbToHex(49.03156666666667, 47.56444444444445, 24.44783333333333))
+      .toBe('#312f18.72a5349');
   });
 
 });
