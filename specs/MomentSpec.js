11cadc2694ad6c22be3acdaae343bee37d1b0b95
Hugh Lee <hammerha@gmail.com>
2016-06-12 21:26:07 +0900

    Fix wrong test

--- specs/MomentSpec.js
+++ specs/MomentSpec.js
@@ -14,8 +14,8 @@ describe('momentjs', function () {
     expect(moment([2016, 4, 13]).format('YYYY-MM-DD')).toBe('2016-05-13');
 
     // [year, month, day, hour, minute, second, millisecond]
-    expect(moment([2010, 1, 14, 15, 25, 50, 125]).format('YYYY-MM-DD hh:mm:ss:ms'))
-      .toBe('2010-01-14 15:25:50.125');
+    expect(moment([2010, 1, 14, 15, 25, 50, 125]).format('YYYY-MM-DD HH:mm:ss.SSS'))
+      .toBe('2010-02-14 15:25:50.125');
   });
 
 });
