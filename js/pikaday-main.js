16b431916b773a91af47369869991553d2d0b0ff
Hugh Lee <hammerha@gmail.com>
2016-06-12 18:48:35 +0900

    Add getting current month and year code of Pikaday

--- js/pikaday-main.js
+++ js/pikaday-main.js
@@ -6,7 +6,7 @@ var picker = new Pikaday({
   field: document.getElementById('datepicker'),
   format: 'D MMM YYYY',
   onSelect: function () {
-    console.log(this.getMoment().format('Do MMMM YYYY'));
+    console.log('selected', this.getMoment().format('DD MMMM YYYY'));
   },
   onDraw: function () {
     /**
@@ -15,8 +15,23 @@ var picker = new Pikaday({
     [10, 11, 12].forEach(function (e, i) {
       $(`.pika-table td[data-day="${e}"] button`).css('color', 'red');
     });
+
+    console.log('onDraw', picker.getMoment().format('M'));
+
+    var $monthLabel = $('.pika-title .pika-label:first');
+    $monthLabel.find('select').remove();
+    var month = $monthLabel.text();
+    console.log('Current Month:', month);
+
+    var $yearLabel = $('.pika-title .pika-label:eq(1)');
+    $yearLabel.find('select').remove();
+    var year = $yearLabel.text();
+    console.log('Current Year:', year);
+  },
+  onOpen : function () {
+    console.log('onOpen', picker.getMoment().format('M'));
   }
 });
 
-
 $('#datepicker').focus();
+
